3e575d837dc00742c7eccf44c0cf27492b553bed
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-01-30 21:48:06 +0100

    Add info about building on Windows

--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@
 from distutils.core import setup
 import py2exe
 
+# to build an executable for Windows, run 'python setup.py py2exe'
+
 setup(windows=['friture.py'], options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"]}})
