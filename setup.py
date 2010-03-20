5bede1239ffc98a6c2c87bb7b61142f6503c24c0
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-03-20 17:55:15 +0100

    Add icon to the Windows exe.

--- setup.py
+++ setup.py
@@ -4,4 +4,5 @@ import py2exe
 
 # to build an executable for Windows, run 'python setup.py py2exe'
 
-setup(windows=['friture.py'], options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"],"excludes":["matplotlib","_ssl","Tkconstants","Tkinter","tcl"]}})
+setup(windows=[{"script":'friture.py', "icon_resources":[(1, "windows-icon.ico")]
+}], options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"],"excludes":["matplotlib","_ssl","Tkconstants","Tkinter","tcl"]}})
