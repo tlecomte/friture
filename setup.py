b5ebc50020b3662798d9cf923b4389cc3fe65da8
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-03-20 17:56:34 +0100

    Add the icons to the source versioning, and fix setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ import py2exe
 
 # to build an executable for Windows, run 'python setup.py py2exe'
 
-setup(windows=[{"script":'friture.py', "icon_resources":[(1, "windows-icon.ico")]
+setup(windows=[{"script":'friture.py', "icon_resources":[(1, "window-icon.ico")]
 }], options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"],"excludes":["matplotlib","_ssl","Tkconstants","Tkinter","tcl"]}})
