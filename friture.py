46abf2fb4fda2e8a2effb842e60a87e407587e77
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-03-28 14:51:07 +0200

    Restore state before restoring geometry.

--- friture.py
+++ friture.py
@@ -226,8 +226,8 @@ class Friture(QtGui.QMainWindow, ):
 		settings.endGroup()
 
 		settings.beginGroup("MainWindow")
-		self.restoreGeometry(settings.value("windowGeometry").toByteArray())
 		self.restoreState(settings.value("windowState").toByteArray())
+		self.restoreGeometry(settings.value("windowGeometry").toByteArray())
 		settings.endGroup()
 		
 		settings.beginGroup("Spectrogram")
