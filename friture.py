0cc90b2b0626ef7c937306434a3e7be71f8d40c2
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-06-04 00:15:18 +0200

    At first launch, do not display the log and stat docks.

--- friture.py
+++ friture.py
@@ -233,6 +233,9 @@ class Friture(QtGui.QMainWindow, ):
 			self.docks += [Dock(self, self.logger, "Dock 2", type = 2)] #spectrum
 			for dock in self.docks:
 				self.addDockWidget(QtCore.Qt.TopDockWidgetArea, dock)
+			# by default, hide the log and statistics docks
+			self.ui.dockWidgetLog.hide()
+			self.ui.dockWidgetStatistics.hide()
 
 		settings.endGroup()
 
