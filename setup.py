76be742e2e249cc84574de688edd42b20568423a
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-06-03 22:47:38 +0200

    Automatically distribute the Qt svg plugin.

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 import py2exe
+from glob import glob
 
 # to build an executable for Windows, run 'python setup.py py2exe'
+
+data_files = [("imageformats", glob(r'C:\Python*\Lib\site-packages\PyQt4\plugins\imageformats\qsvg4.dll'))]
 
 setup(windows=[{"script":'friture.py', "icon_resources":[(1, "window-icon.ico")]}],
 	  options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"],
 						 "excludes":["matplotlib","_ssl","Tkconstants","Tkinter","tcl"],
-						 "dll_excludes":["powrprof.dll"]}})
+						 "dll_excludes":["powrprof.dll"]}},
+	  data_files=data_files)
