97867c37d1c44a937753a842dd70d0fe9f151ab5
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-06-03 22:38:06 +0200

    Exclude powrprof.dll in py2exe distribution.

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ import py2exe
 
 # to build an executable for Windows, run 'python setup.py py2exe'
 
-setup(windows=[{"script":'friture.py', "icon_resources":[(1, "window-icon.ico")]
-}], options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"],"excludes":["matplotlib","_ssl","Tkconstants","Tkinter","tcl"]}})
+setup(windows=[{"script":'friture.py', "icon_resources":[(1, "window-icon.ico")]}],
+	  options={"py2exe":{"includes":["sip", "PyQt4.QtSvg"],
+						 "excludes":["matplotlib","_ssl","Tkconstants","Tkinter","tcl"],
+						 "dll_excludes":["powrprof.dll"]}})
