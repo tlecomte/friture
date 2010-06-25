4249b519411c7561658ea0a04c5ebaec6d3def89
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2010-06-25 20:16:43 +0200

    Finish and cleanup implementation of an octave (or fraction of octave) filter bank

--- cochlear.py
+++ cochlear.py
@@ -44,21 +44,21 @@ def MakeERBFilters(fs, numChannels, lowFreq):
 	ERB = ((cf/EarQ)**order + minBW**order)**(1./order)
 	B = 1.019*2*pi*ERB
 	gain = abs((-2*exp(4*1j*cf*pi*T)*T + \
-	       2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
-	       (cos(2*cf*pi*T) - sqrt(3. - 2.**(3./2.))* \
-	       sin(2*cf*pi*T))) * \
-	       (-2*exp(4*1j*cf*pi*T)*T + \
-	       2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
-	       (cos(2*cf*pi*T) + sqrt(3. - 2.**(3./2.)) * \
-	       sin(2*cf*pi*T)))* \
-	       (-2*exp(4*1j*cf*pi*T)*T + \
-	       2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
-	       (cos(2*cf*pi*T) - \
-	       sqrt(3. + 2.**(3./2.))*sin(2*cf*pi*T))) * \
-	       (-2*exp(4*1j*cf*pi*T)*T + 2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
-	       (cos(2*cf*pi*T) + sqrt(3. + 2.**(3./2.))*sin(2*cf*pi*T))) / \
-	       (-2 / exp(2*B*T) - 2*exp(4*1j*cf*pi*T) + \
-	       2*(1 + exp(4*1j*cf*pi*T))/exp(B*T))**4)
+		   2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
+		   (cos(2*cf*pi*T) - sqrt(3. - 2.**(3./2.))* \
+		   sin(2*cf*pi*T))) * \
+		   (-2*exp(4*1j*cf*pi*T)*T + \
+		   2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
+		   (cos(2*cf*pi*T) + sqrt(3. - 2.**(3./2.)) * \
+		   sin(2*cf*pi*T)))* \
+		   (-2*exp(4*1j*cf*pi*T)*T + \
+		   2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
+		   (cos(2*cf*pi*T) - \
+		   sqrt(3. + 2.**(3./2.))*sin(2*cf*pi*T))) * \
+		   (-2*exp(4*1j*cf*pi*T)*T + 2*exp(-(B*T) + 2*1j*cf*pi*T)*T* \
+		   (cos(2*cf*pi*T) + sqrt(3. + 2.**(3./2.))*sin(2*cf*pi*T))) / \
+		   (-2 / exp(2*B*T) - 2*exp(4*1j*cf*pi*T) + \
+		   2*(1 + exp(4*1j*cf*pi*T))/exp(B*T))**4)
 	
 	feedback = zeros((len(cf), 9))
 	forward = zeros((len(cf), 5))
@@ -91,114 +91,55 @@ def ERBFilterBank(forward, feedback, x):
 		y[i,:] = lfilter(forward[i,:], feedback[i,:], x)
 	return y
 
-#1/3 octave (or 1/10 decade) in audio means (ISO R 266 and ANSI S1.6-1984):
-#fc[n]=10*e^(n/10) 15<=n<=43
-#or
-#fc[k]=2^k/3*1000Hz
-
-#Nominal freq: 31.5 40 50 63 80 100 125 160 200 250 315 400 500 630 800 1000 1250
-#1600 2000 Hz, etc.
-#The upper and lower band edges are:
-#fcl=sqrt(fc[k-1]*fc[k])
-#fch=sqrt(fc[k]*fc[k+1])
+# Nominal frequencies: 31.5 40 50 63 80 100 125 160 200 250 315 400 500 630 800 1000 1250
+# 1600 2000 Hz, etc.
 def octave_frequencies(Nbands, BandsPerOctave):
 	f0 = 1000. # audio reference frequency is 1 kHz
-	fs = 44100 # sampling rate
 	
 	b = 1./BandsPerOctave
 	
+	imax = Nbands/2
 	if Nbands%2 == 1:
-		i = arange(-Nbands/2, Nbands/2 + 1)
+		i = arange(-imax, imax + 1)
 	else:
-		i = arange(-Nbands/2, Nbands/2) + 0.5
+		i = arange(-imax, imax) + 0.5
 	
 	fi = f0 * 2**(i*b)
 	f_low = fi * sqrt(2**(-b))
 	f_high = fi * sqrt(2**b)
 	
-	wi = fi/fs
-	w_low = f_low/fs
-	w_high = f_high/fs
-	
-	return fi, f_low, f_high, wi, w_low, w_high
+	return fi, f_low, f_high
 
-def octave_filters(Nchannels):
+def octave_filters(Nbands, BandsPerOctave):
 	# Bandpass Filter Generation
+	pbrip = .5	# Pass band ripple
+	sbrip = 30	# Stop band rejection
 
-	fstart = 0.05      # Start frequency
-	fend = 0.95        # End frequency
-	pbrip = .5         # Pass band ripple
-	sbrip = 30         # Stop band rejection
+	fi, f_low, f_high = octave_frequencies(Nbands, BandsPerOctave)
 
-	Nbands = 7
-	BandsPerOctave = 1
-	print octave_frequencies(Nbands, BandsPerOctave)
+	fs = 44100 # sampling rate
+	wi = fi/(fs/2.) # normalized frequencies
+	w_low = f_low/(fs/2.)
+	w_high = f_high/(fs/2.)
 
-	K = arange(fstart, fend, (fend-fstart)/Nchannels)
 	B = []
 	A = []
 	
-	# For each frequency bin
-	for k in K:
-		# Scale fails by default
-		fail = 1
-		scale = 1
-
-		while fail == 1:
-			#The scale has to be adjusted so that filter coefficients are
-			# -1.0<coeff<1.0
-			#Scaling performed is 2^(-scale)
+	# For each band
+	for w, wl, wh in zip(wi, w_low, w_high):
+		# normalized frequency vector
+		freq = [wl, wh]
 			
-			#For lowpass, set equal to normalized Freq (cutoff/(Fs/2))
-			#For bandpass, set equal to normalized Freq vector ([low high]/(Fs/2))
-			freq = [k, k+(fend-fstart)/(2*Nchannels)]
-			
-			#Filter order:
-			#	use 2,4 for lowpass
-			#		1,2 for bandpass
-			#NOTE that for a bandpass filter (order) poles are generated for the high
-			#and low cutoffs, so the total order is (order)*2
-			order = 2
+		#Filter order
+		order = 2
 	
-			#could also use butter, or cheby1 or cheby2 or besself
-			# but note that besself is lowpass only!
-			[b, a] = ellip(order, pbrip, sbrip, freq, btype='bandpass')
-	
-			b = b * (2**-scale)
-			a = -a * (2**-scale)
-	
-			# Keep increasing scale until coeffs are safe
-			if sum(abs(b) >= 1) | sum(abs(a) >= 1):
-				fail = 1
-				scale = scale + 1
-				print 'Retrying %f with new scale factor %d' %(k, scale)
-			else:
-				fail = 0
-	
-		ordera = order*len(freq)
-		if ordera==2:
-			scstr = 'IIR2 filter'
-		elif ordera==4:
-			scstr = 'IIR4 filter'
-		else:
-			error('order*length(freq) must equal 2 or 4')
-	
+		# could be another IIR filter
 		[b, a] = ellip(order, pbrip, sbrip, freq, btype='bandpass')
 		
 		B += [b]
 		A += [a]
 		
 	return [B, A]
-
-	##sampling rate on DE2 board
-	#Fs = 8000
-	#[fresponse, ffreq] = freqz(b,a,1000)
-	#plot(ffreq/pi*Fs/2,abs(fresponse))
-	#b = fix((b*(2**-scale))*2**16)/2**16
-	#a = fix((a*(2**-scale))*2**16)/2**16
-	#[fresponse, ffreq] = freqz(b,a,1000)
-	#plot(ffreq/pi*Fs/2,abs(fresponse))
-	#legend('exact','scaled 16-bit')
 
 def octave_filter_bank(forward, feedback, x):
 	# This function filters the waveform x with the array of filters
@@ -213,34 +154,36 @@ def octave_filter_bank(forward, feedback, x):
 
 # main() is a test function
 def main():
-    from matplotlib.pyplot import semilogx, plot, show, xlim, ylim
-    from numpy.fft import fft, fftfreq
-    from numpy import log10, linspace
+	from matplotlib.pyplot import semilogx, plot, show, xlim, ylim
+	from numpy.fft import fft, fftfreq
+	from numpy import log10, linspace
 
-    N = 2048
-    fs = 44100.
-    Nchannels = 20
-    low_freq = 20.
+	N = 2048*2*2
+	fs = 44100.
+	Nchannels = 20
+	low_freq = 20.
 
-    impulse = zeros(N)
-    impulse[0] = 1
-    #impulse = sin(linspace(0, 600*pi, N))
+	impulse = zeros(N)
+	impulse[0] = 1
+	#impulse = sin(linspace(0, 600*pi, N))
 
-    [ERBforward, ERBfeedback] = MakeERBFilters(fs, Nchannels, low_freq)
-    y = ERBFilterBank(ERBforward, ERBfeedback, impulse)
-    
-    [B, A] = octave_filters(Nchannels)
-    y = octave_filter_bank(B, A, impulse)
+	[ERBforward, ERBfeedback] = MakeERBFilters(fs, Nchannels, low_freq)
+	y = ERBFilterBank(ERBforward, ERBfeedback, impulse)
 
-    response = 20.*log10(abs(fft(y)))
-    freqScale = fftfreq(N, 1./fs)
+	Nbands = 7
+	BandsPerOctave = 1
+	[B, A] = octave_filters(Nbands, BandsPerOctave)
+	y = octave_filter_bank(B, A, impulse)
 
-    for i in range(0, response.shape[0]):
-            semilogx(freqScale[0:N/2],response[i, 0:N/2])
-    xlim(fs/100, fs)
-    ylim(-70, 10)
+	response = 20.*log10(abs(fft(y)))
+	freqScale = fftfreq(N, 1./fs)
 
-    show()
-    
+	for i in range(0, response.shape[0]):
+			semilogx(freqScale[0:N/2],response[i, 0:N/2])
+	xlim(fs/2000, fs)
+	ylim(-70, 10)
+
+	show()
+	
 if __name__ == "__main__":
-    main()
+	main()
