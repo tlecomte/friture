82874a6ff84f6186aa9e4cb47542788bf02fee90
“Timothée <timothee.lecomte@lpa.ens.fr>
2010-12-28 14:35:28 +0100

    Add Cython machinery to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
-import py2exe
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
 from glob import glob
 
+try:
+	import py2exe
+except ImportError:
+	print "Cannot find py2exe"
+
 # to build an executable for Windows, run 'python setup.py py2exe'
+# to build the cython extension(s), run 'python setup.py build_ext --inplace'
 
 #include the QT svg plugin to render the icons
 #include the filter coefficients in the pickle file
@@ -20,6 +27,12 @@ dll_excludes = ["powrprof.dll"]
 #manually exclude python libraries that py2exe fails to detect
 includes = ["sip", "PyQt4.QtSvg"]
 
-setup(windows=[{"script":'friture.py', "icon_resources":[(1, "window-icon.ico")]}],
-	  options={"py2exe":{"includes":includes, "excludes":excludes, "dll_excludes":dll_excludes}},
-	  data_files=data_files)
+ext_modules = [Extension("exp_smoothing_conv", ["exp_smoothing_conv.pyx"])]
+
+setup(name = "Friture",
+	windows = [{"script":'friture.py', "icon_resources":[(1, "window-icon.ico")]}],
+	options = {"py2exe":{"includes":includes, "excludes":excludes, "dll_excludes":dll_excludes}},
+	data_files=data_files,
+	cmdclass = {"build_ext": build_ext},
+	ext_modules = ext_modules
+	)
