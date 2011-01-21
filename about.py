de46e63f396dc73803980c1ce89f633401f2a277
“Timothée <timothee.lecomte@lpa.ens.fr>
2011-01-21 14:15:24 +0100

    Another fix for the resource file move

--- about.py
+++ about.py
@@ -18,7 +18,7 @@
 # along with Friture.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt4 import QtGui, QtCore
-import resource_rc
+import friture_rc
 
 aboutText = """
 <p> Friture is an application for real-time audio analysis.
