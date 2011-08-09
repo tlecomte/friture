82fda728138b8ba560353704c28a84d36e4682fb
Timothee Lecomte <timothee.lecomte@lpa.ens.fr>
2011-08-09 22:26:59 +0200

    Fix and cleanup the sandbox file for FIR/IIR comparison

--- sandbox/filter_compare_fir_iir.py
+++ sandbox/filter_compare_fir_iir.py
@@ -1,21 +1,18 @@
-from pylab import *
-from scipy.signal.filter_design import ellip
-from scipy.signal import remez, freqz, iirdesign
+from matplotlib import pylab as plt
+import numpy as np
+from scipy.signal import remez, iirdesign
 from scipy.signal.signaltools import lfilter
-
-figure()
+from numpy.fft import fft, fftshift, fftfreq
 
 N = 2**16
 fs = 44100.
-x = arange(0,N)/fs
-f = 18000.
+x = np.arange(0,N)/fs
+f = 1800.
 #signal
-y = sin(2*pi*x*f)
+y = np.sin(2*np.pi*x*f)
 
 #window
-Nw = 2**13
-window = zeros(N)
-window[:Nw] = hamming(Nw)
+window = np.hanning(N)
 
 # windowed signal
 y *= window
@@ -23,33 +20,37 @@ y *= window
 (b_iir, a_iir) = iirdesign(0.49, 0.51, 0.05, 70, analog=0, ftype='ellip', output='ba')
 print "IIR coeff created", len(b_iir), len(a_iir)
 
-Ntaps = 256
+Ntaps = 512
 b_fir = remez(numtaps=Ntaps, bands=[0., 0.49/2., 0.51/2., 1./2.], desired=[1.,0.])#, maxiter=250, grid_density=64)
 a_fir = [1.]
 print "FIR coeff created", len(b_fir), len(a_fir)
 
-yf_fir, zf = lfilter(b_fir, a_fir, y, zi=zeros(max(len(a_fir),len(b_fir))-1))
+yf_fir, zf = lfilter(b_fir, a_fir, y, zi=np.zeros(max(len(a_fir),len(b_fir))-1))
+yf_iir, zf = lfilter(b_iir, a_iir, y, zi=np.zeros(max(len(a_iir),len(b_iir))-1))
 
-impulse = zeros(N); impulse[0] = 1.
-yf_imp_fir, zf = lfilter(b_fir, a_fir, impulse, zi=zeros(max(len(a_fir),len(b_fir))-1))
-yf_imp_iir, zf = lfilter(b_iir, a_iir, impulse, zi=zeros(max(len(a_iir),len(b_iir))-1))
+impulse = np.zeros(N); impulse[N/2] = 1.
+yf_imp_fir, zf = lfilter(b_fir, a_fir, impulse, zi=np.zeros(max(len(a_fir),len(b_fir))-1))
+yf_imp_iir, zf = lfilter(b_iir, a_iir, impulse, zi=np.zeros(max(len(a_iir),len(b_iir))-1))
 
-clf()
+plt.figure()
 
-subplot(311)
-title("original and filtered signals")
-plot(x, y)
-plot(x, yf_fir)#; plot(x[-1]+x, yf2); plot(2*x[-1]+x, yf3)
+plt.subplot(311)
+plt.title("original and filtered signals")
+plt.plot(x, y)
+plt.plot(x, yf_fir)
+plt.plot(x, yf_iir)
 
-subplot(312)
-title("original and filtered frequency distributions")
-semilogx(fftshift(fftfreq(N,1./fs))[N/2:], fftshift(20.*log(abs(fft(y))))[N/2:])
-semilogx(fftshift(fftfreq(N,1./fs))[N/2:], fftshift(20.*log(abs(fft(yf_fir))))[N/2:])
-ylim(ymin=1e-6)
+plt.subplot(312)
+plt.title("original and filtered frequency distributions")
+freqScale = fftshift(fftfreq(N,1./fs))[N/2:]
+plt.semilogx(freqScale, fftshift(20. * np.log10(abs(fft(y*window))))[N/2:])
+plt.semilogx(freqScale, fftshift(20. * np.log10(abs(fft(yf_fir*window))))[N/2:])
+plt.semilogx(freqScale, fftshift(20. * np.log10(abs(fft(yf_iir*window))))[N/2:])
 
-#subplot(313); w, h = freqz(b, a, worN=N); loglog(w, abs(h)**2)
-subplot(313)
-title("filter impulse response")
-semilogx(fftshift(fftfreq(N,1./fs))[N/2:], fftshift(20.*log(abs(fft(yf_imp_fir))))[N/2:])
-semilogx(fftshift(fftfreq(N,1./fs))[N/2:], fftshift(20.*log(abs(fft(yf_imp_iir))))[N/2:])
-show()
+plt.subplot(313)
+plt.title("filter impulse response")
+plt.semilogx(freqScale, fftshift(20. * np.log10(abs(fft(impulse*window))))[N/2:], label="signal")
+plt.semilogx(freqScale, fftshift(20. * np.log10(abs(fft(yf_imp_fir*window))))[N/2:], label="FIR filt.")
+plt.semilogx(freqScale, fftshift(20. * np.log10(abs(fft(yf_imp_iir*window))))[N/2:], label="IIR filt.")
+plt.legend(loc='lower left')
+plt.show()
