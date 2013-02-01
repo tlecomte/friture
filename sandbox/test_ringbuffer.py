314f2f67b21803871e68eed7f4162fb8fe09c886
Timothee Lecomte <lecomte.timothee@gmail.com>
2013-02-01 21:24:54 +0100

    Add testing for empty push in ringbuffer test code

--- sandbox/test_ringbuffer.py
+++ sandbox/test_ringbuffer.py
@@ -2,7 +2,7 @@ import numpy as np
 from friture.ringbuffer import RingBuffer
 import matplotlib.pyplot as plt
 
-Ns = int(1e6)
+Ns = int(1e4)
 
 x = np.arange(Ns)
 x.shape = (1,Ns)
@@ -14,9 +14,22 @@ print "Nb =", Nb
 rb = RingBuffer()
 rb.data(Ns) # grow the buffer as needed
 
-print "pushing"
-for i in range(Nb):
+# split in two to test zero-length insertions
+Nb0 = Nb/2
+
+print "Nb0 =", Nb0, "Nb1 =", Nb - Nb0
+
+print "pushing first parts"
+for i in range(Nb0):
 	rb.push(x[:,i*l:(i+1)*l])
+
+print "pushing an empty array", x[:,0:0].shape
+rb.push(x[:,0:0])
+
+print "pushing second parts"
+for i in range(Nb0,Nb):
+	rb.push(x[:,i*l:(i+1)*l])
+
 print "pushing leftovers"
 rb.push(x[:,Nb*l:])
 
