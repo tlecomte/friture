81e2c769072ba04c2cac742e8a1886ebbb438a3c
Timothée Lecomte <lecomte.timothee@gmail.com>
2015-02-11 22:11:09 +0100

    Update version number, 0.13

--- friture/__init__.py
+++ friture/__init__.py
@@ -18,5 +18,5 @@
 # along with Friture.  If not, see <http://www.gnu.org/licenses/>.
 
 # version and date
-__version__ = "0.12"
-__releasedate__ = "2013-06-20"
+__version__ = "0.13"
+__releasedate__ = "2015-02-11"
