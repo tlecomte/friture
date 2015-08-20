145e1bbfbe64839bea06fb96be3079951dd28642
Timothée Lecomte <lecomte.timothee@gmail.com>
2015-08-20 23:03:42 +0200

    Sweep generator: avoid phase jumps at sweep restart and when settings change

--- friture/generators/sweep.py
+++ friture/generators/sweep.py
@@ -28,8 +28,10 @@ class SweepGenerator:
     def __init__(self, parent, logger):
         self.f1 = 20.
         self.f2 = 22000.
-        self.T = 1.
-        self.L, self.K = self.computeKL(self.f1, self.f2, self.T)
+        self.Tuser = 1.
+        self.L, self.K, self.T = self.computeParams(self.f1, self.f2, self.Tuser)
+        self.timeoffset = 0.
+        self.nextParams = None
 
         self.settings = SettingsWidget(parent, logger)
         self.settings.spinBox_sweep_startfrequency.valueChanged.connect(self.setf1)
@@ -39,31 +41,70 @@ class SweepGenerator:
     def settingsWidget(self):
         return self.settings
 
-    def computeKL(self, f1, f2, T):
+    def computeParams(self, f1, f2, T):
+        # adjust T so that we have an integer number of periods
+        # we want phase_max to be a multiple of 2*np.pi
+        # phase_max = 2*np.pi*f1*T/np.log(f2/f1)*(f2/f1 - 1.)
+        # phase_max = N*2*np.pi
+        # N = f1*T/np.log(f2/f1)*(f2/f1 - 1.)
+        Tmult = np.log(f2/f1)/(f1*(f2/f1 - 1.))
+        if T >= Tmult:
+            T = np.round(T/Tmult)*Tmult
+        else:
+            T = np.ceil(T/Tmult)*Tmult
+
         w1 = 2*np.pi*f1
         w2 = 2*np.pi*f2
         K = w1*T/np.log(w2/w1)
         L = T/np.log(w2/w1)
-        return L, K
+        return L, K, T
 
     def setf1(self, f1):
-        self.f1 = f1
-        self.L, self.K = self.computeKL(self.f1, self.f2, self.T)
+        if self.f1 != f1:
+            self.f1 = f1
+            L, K, T = self.computeParams(self.f1, self.f2, self.Tuser)
+            self.nextParams = [L, K, T]
 
     def setf2(self, f2):
-        self.f2 = f2
-        self.L, self.K = self.computeKL(self.f1, self.f2, self.T)
+        if self.f2 != f2:
+            self.f2 = f2
+            L, K, T = self.computeParams(self.f1, self.f2, self.Tuser)
+            self.nextParams = [L, K, T]
 
     def setT(self, T):
-        self.T = T
-        self.L, self.K = self.computeKL(self.f1, self.f2, self.T)
+        if self.Tuser != T:
+            self.Tuser = T
+            L, K, T = self.computeParams(self.f1, self.f2, self.Tuser)
+            self.nextParams = [L, K, T]
 
     def signal(self, t):
         # https://ccrma.stanford.edu/realsimple/imp_meas/Sine_Sweep_Measurement_Theory.html
 
         #f = (self.f2 - self.f1)*(1. + np.sin(2*np.pi*t/self.T))/2. + self.f1
         #return np.sin(2*np.pi*t*f)
-        return np.sin(self.K*(np.exp(t%self.T/self.L) - 1.))
+
+        result = np.cos(self.K*(np.exp((t - self.timeoffset)%self.T/self.L) - 1.))
+
+        if self.nextParams != None:
+            # we have new params to put in place
+            # do it at the first max
+
+            diff = result[1:] - result[:-1]
+            maxdetection = 0.*diff[:-1] + (diff[1:] < 0.)*(diff[:-1] > 0.)*1.
+
+            maxdetections = np.argwhere(maxdetection != 0.)
+
+            if len(maxdetections) > 0:
+                firstmaxpos = np.argwhere(maxdetection != 0.)[0] + 1
+
+                # the first samples use the previous parameters
+                # the next samples use the new parameters
+                [self.L, self.K, self.T] = self.nextParams
+                self.nextParams = None
+                self.timeoffset = t[firstmaxpos]
+                result[firstmaxpos:] = np.cos(self.K*(np.exp((t[firstmaxpos:] - self.timeoffset)%self.T/self.L) - 1.))
+
+        return result
 
 class SettingsWidget(QtWidgets.QWidget):
     def __init__(self, parent, logger):
