4f857893c47be7dcf2e6fdbc6bba0254d7078fe1
Timothée Lecomte <lecomte.timothee@gmail.com>
2016-02-28 15:40:31 +0100

    Download page is now based on the Github releases

---
+++ js/controllers.js
@@ -0,0 +1,19 @@
+var fritureApp = angular.module('fritureApp', []);
+
+fritureApp.controller('ReleasesCtrl', function ($scope, $http) {
+  $http.get('https://api.github.com/repos/tlecomte/friture/releases/latest').success(function(data) {
+    $scope.latest = data;
+    $scope.windowsAsset = data.assets.filter(function(asset) { return asset.name.match(/.exe$/); })[0];
+    $scope.macAsset = data.assets.filter(function(asset) { return asset.name.match(/.dmg$/); })[0];
+  });
+});
+
+fritureApp.filter('bytes', function() {
+    return function(bytes, precision) {
+        if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+        if (typeof precision === 'undefined') precision = 1;
+        var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+            number = Math.floor(Math.log(bytes) / Math.log(1024));
+        return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+    }
+});
