cc7fa36fca62cfe194715ee703fdb08c90136948
Timothée Lecomte <lecomte.timothee@gmail.com>
2017-12-17 16:32:54 +0100

    Version 0.25

--- friture/__init__.py
+++ friture/__init__.py
@@ -18,10 +18,10 @@
 # along with Friture.  If not, see <http://www.gnu.org/licenses/>.
 
 # version and date
-__version__ = "0.24"
+__version__ = "0.25"
 # NSIS (because of Windows) requires the version to be in the format X.X.X.X
 # the following line is read by the installer
-__versionXXXX__ = "0.24.0.0"
-__releasedate__ = "2016-12-31"
+__versionXXXX__ = "0.25.0.0"
+__releasedate__ = "2017-12-17"
 
 __all__ = ["plotting"]
