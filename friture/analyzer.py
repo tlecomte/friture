d1333972bc253a00912ca9ecd38f316177b99e81
Timothée Lecomte <lecomte.timothee@gmail.com>
2018-01-16 20:13:39 +0100

    Add missing flush() method to the stdout/stderr redirect implementations

--- friture/analyzer.py
+++ friture/analyzer.py
@@ -244,17 +244,20 @@ def qt_message_handler(mode, context, message):
         logger.debug(message)
 
 class StreamToLogger(object):
-   """
-   Fake file-like stream object that redirects writes to a logger instance.
-   """
-   def __init__(self, logger, log_level=logging.INFO):
-      self.logger = logger
-      self.log_level = log_level
-      self.linebuf = ''
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+        self.linebuf = ''
 
-   def write(self, buf):
-      for line in buf.rstrip().splitlines():
-         self.logger.log(self.log_level, line.rstrip())
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass
 
 def main():
     # make the Python warnings go to Friture logger
