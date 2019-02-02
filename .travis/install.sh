f62c7592181e98985d67fa6f020460687ae567fe
Timothée Lecomte <lecomte.timothee@gmail.com>
2019-02-02 22:06:07 +0100

    Add more Linux packaging

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+source multibuild/common_utils.sh
+source multibuild/travis_steps.sh
+before_install
+
+which python3
+python3 -c 'import sys; print(sys.version)'
+
+pip3 install -r requirements.txt
+
+# py2app chokes when the extensions are not built explicitely
+python3 setup.py build_ext --inplace
+
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     # Macos
-    source multibuild/common_utils.sh
-    source multibuild/travis_steps.sh
-    before_install
-
-    which python3
-    python3 -c 'import sys; print(sys.version)'
-
-    pip3 install -r requirements.txt
-
-    # py2app chokes when the extensions are not built explicitely
-    python3 setup.py build_ext --inplace
     pip3 install -U "py2app==0.14"
     python3 setup.py py2app
     # prepare a dmg out of friture.app
@@ -23,6 +24,14 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     du -hs $ARTIFACT_FILENAME
 else
     # Linux
-    bash -ex pkg2appimage recipes/$RECIPE.yml
+    pyinstaller friture.spec -y --log-level=DEBUG
+
+    ls -la dist
+
+    wget -q https://github.com/AppImage/AppImages/raw/master/pkg2appimage -O ./pkg2appimage
+    chmod a+x ./pkg2appimage
+    bash -ex pkg2appimage recipes/friture.yml
+
+    ls -la out
 fi
 
