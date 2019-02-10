b74fff747accb84c4c2f314f3f19e60b380adb23
Timothée Lecomte <lecomte.timothee@gmail.com>
2019-02-10 19:24:51 +0100

    (Travis Linux) install desktop-file-utils, dependency of pkg2appimage

--- .travis/install.sh
+++ .travis/install.sh
@@ -28,6 +28,7 @@ else
     # Linux
     sudo apt-get update
     sudo apt-get install -y libportaudio0
+    sudo apt-get install -y desktop-file-utils # for desktop-file-validate, used by pkg2appimage
 
     # about pep517, see https://github.com/pypa/pip/issues/6163
     pip3 install -U pyinstaller --no-use-pep517
