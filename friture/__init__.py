941170a97e2dd4e308556020719499fc8e283d71
Timothée Lecomte <lecomte.timothee@gmail.com>
2019-02-15 19:29:16 +0100

    Update version to 0.37 to fix the PyPI release

--- friture/__init__.py
+++ friture/__init__.py
@@ -18,10 +18,10 @@
 # along with Friture.  If not, see <http://www.gnu.org/licenses/>.
 
 # version and date
-__version__ = "0.36"
+__version__ = "0.37"
 # NSIS (because of Windows) requires the version to be in the format X.X.X.X
 # the following line is read by the installer
-__versionXXXX__ = "0.36.0.0"
-__releasedate__ = "2019-02-01"
+__versionXXXX__ = "0.37.0.0"
+__releasedate__ = "2019-02-15"
 
 __all__ = ["plotting"]
