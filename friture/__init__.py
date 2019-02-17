a3a0a8ed95ef45f15d50fb6a060269a2c870829f
Timothée Lecomte <lecomte.timothee@gmail.com>
2019-02-17 22:19:46 +0100

    Update version to 0.38

--- friture/__init__.py
+++ friture/__init__.py
@@ -18,10 +18,10 @@
 # along with Friture.  If not, see <http://www.gnu.org/licenses/>.
 
 # version and date
-__version__ = "0.37"
+__version__ = "0.38"
 # NSIS (because of Windows) requires the version to be in the format X.X.X.X
 # the following line is read by the installer
-__versionXXXX__ = "0.37.0.0"
-__releasedate__ = "2019-02-15"
+__versionXXXX__ = "0.38.0.0"
+__releasedate__ = "2019-02-17"
 
 __all__ = ["plotting"]
