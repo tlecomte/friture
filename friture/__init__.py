91029ce5dd393f77a3665be9f75d2490df1ba03d
Timothée Lecomte <lecomte.timothee@gmail.com>
2019-12-08 17:16:00 +0100

    Version 0.41

--- friture/__init__.py
+++ friture/__init__.py
@@ -18,7 +18,7 @@
 # along with Friture.  If not, see <http://www.gnu.org/licenses/>.
 
 # version and date
-__version__ = "0.40"
-__releasedate__ = "2019-10-30"
+__version__ = "0.41"
+__releasedate__ = "2019-12-08"
 
 __all__ = ["plotting"]
