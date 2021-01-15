44ad3661a9fe40421f097adea8f416ea621d5c89
Timothée Lecomte <lecomte.timothee@gmail.com>
2021-01-15 22:35:57 +0100

    Update pyinstaller to 4.2
    Part of the fix to get Friture properly working on recent macos Big Sur versions.
    See #154

--- winbuild.ps1
+++ winbuild.ps1
@@ -85,7 +85,7 @@ Write-Host "==========================================="
 Write-Host "Installing pyinstaller"
 Write-Host "==========================================="
 
-& pip install -U pyinstaller==4.1
+& pip install -U pyinstaller==4.2
 
 Write-Host ""
 Write-Host "==========================================="
