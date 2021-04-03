a4594406b52090d5eda38c2fb8be569759661d68
Joost <Joost@tmmrman.nl>
2021-04-03 22:45:28 +0200

    Handle invalid docknames

--- friture/analyzer.py
+++ friture/analyzer.py
@@ -198,6 +198,8 @@ class Friture(QMainWindow, ):
         # 2. remove any level widget
         if settings.contains("Docks/dockNames"):
             docknames = settings.value("Docks/dockNames", [])
+            if docknames == None:
+            	docknames = []
             newDockNames = []
             for dockname in docknames:
                 widgetType = settings.value("Docks/" + dockname + "/type", 0, type=int)
