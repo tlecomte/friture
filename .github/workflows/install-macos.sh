da4e21ff86179ba369ef76d0ee2ed19c91c2871e
Timothée Lecomte <lecomte.timothee@gmail.com>
2022-03-12 19:02:15 +0100

    fix(macos): codesign despite Qt5 folder names
    PyInstaller will try to codesign friture.app, but will fail because of the Qt5 file structure (this is visible in the build logs).
    
    See https://github.com/pyinstaller/pyinstaller/wiki/Recipe-OSX-Code-Signing-Qt
    
    So we fix the folder names and then sign again manually.

--- .github/workflows/install-macos.sh
+++ .github/workflows/install-macos.sh
@@ -26,6 +26,14 @@ ls -la /usr/local/lib/libportaudio.dylib
 ls -la /usr/local/Cellar/portaudio/*/lib/libportaudio*.dylib
 ls -la dist/friture.app/Contents/MacOS/_sounddevice_data/portaudio-binaries
 
+# PyInstaller will try to codesign friture.app, but will fail because of the Qt5 file structure
+# (this is visible in the logs)
+# see https://github.com/pyinstaller/pyinstaller/wiki/Recipe-OSX-Code-Signing-Qt
+# so we fix the folder names and then sign again manually
+python3 installer/fix_app_qt_folder_names_for_codesign.py dist/friture.app
+codesign -s - --force --all-architectures --timestamp --deep dist/friture.app
+codesign -dv dist/friture.app
+
 # prepare a dmg out of friture.app
 export ARTIFACT_FILENAME=friture-$(python3 -c 'import friture; print(friture.__version__)')-$(date +'%Y%m%d').dmg
 echo $ARTIFACT_FILENAME
