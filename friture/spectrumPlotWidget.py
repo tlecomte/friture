4226b0298f5ccb14d3e31188c68a71d56c70ba8b
Alexis Rossfelder <contact.liteapplication@gmail.com>
2024-03-15 22:17:56 +0100

    Show pitch on the tracker

--- friture/spectrumPlotWidget.py
+++ friture/spectrumPlotWidget.py
@@ -18,13 +18,15 @@ from friture.store import GetStore
 from friture.qml_tools import qml_url, raise_if_error
 
 # The peak decay rates (magic goes here :).
-PEAK_DECAY_RATE = 1.0 - 3E-6
+PEAK_DECAY_RATE = 1.0 - 3e-6
 # Number of cycles the peak stays on hold before fall-off.
 PEAK_FALLOFF_COUNT = 32  # default : 16
 
+
 class Baseline(Enum):
-    PLOT_BOTTOM = 1,
+    PLOT_BOTTOM = (1,)
     DATA_ZERO = 2
+
 
 class SpectrumPlotWidget(QtWidgets.QWidget):
 
@@ -47,7 +49,9 @@ class SpectrumPlotWidget(QtWidgets.QWidget):
         self._spectrum_data.vertical_axis.name = "PSD (dB)"
         self._spectrum_data.vertical_axis.setTrackerFormatter(lambda x: "%.1f dB" % (x))
         self._spectrum_data.horizontal_axis.name = "Frequency (Hz)"
-        self._spectrum_data.horizontal_axis.setTrackerFormatter(lambda x: "%.0f Hz" % (x))
+        self._spectrum_data.horizontal_axis.setTrackerFormatter(
+            self.trackerFormatterPitch
+        )
 
         self._spectrum_data.vertical_axis.setRange(0, 1)
         self._spectrum_data.horizontal_axis.setRange(0, 22000)
@@ -71,7 +75,9 @@ class SpectrumPlotWidget(QtWidgets.QWidget):
         self.quickWidget = QQuickWidget(engine, self)
         self.quickWidget.statusChanged.connect(self.on_status_changed)
         self.quickWidget.setResizeMode(QQuickWidget.SizeRootObjectToView)
-        self.quickWidget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.quickWidget.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding
+        )
         self.quickWidget.setSource(qml_url("Spectrum.qml"))
 
         raise_if_error(self.quickWidget)
@@ -137,17 +143,25 @@ class SpectrumPlotWidget(QtWidgets.QWidget):
     def set_baseline_dataUnits(self, baseline):
         self._baseline = Baseline.DATA_ZERO
 
-
     def freq_to_note(self, freq: float) -> str:
         if freq == 0:
             return ""
         # number of semitones from C4
         # A4 = 440Hz and is 9 semitones above C4
-        semitone = round(np.log2(freq/440) * 12) + 9
+        semitone = round(np.log2(freq / 440) * 12) + 9
         octave = int(np.floor(semitone / 12)) + 4
         notes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
-        return f'{notes[semitone % 12]}{octave}'
+        return f"{notes[semitone % 12]}{octave}"
 
+    def trackerFormatterPitch(self, x):
+        try:
+            note = self.freq_to_note(x)
+        except ValueError:
+            return ""
+        if x < 1000:
+            return "%d Hz (%s)" % (int(x), note)
+        else:
+            return "%.1f kHz (%s)" % (x / 1000, note)
 
     def setdata(self, x, y, fmax, fpitch):
         if not self.paused:
@@ -155,36 +169,46 @@ class SpectrumPlotWidget(QtWidgets.QWidget):
                 text = "%.1f Hz" % (fmax)
             else:
                 text = "%d Hz" % (np.rint(fmax))
-            self._spectrum_data.setFmax(text, self.normHorizontalScaleTransform.toScreen(fmax))
+            self._spectrum_data.setFmax(
+                text, self.normHorizontalScaleTransform.toScreen(fmax)
+            )
             self._spectrum_data.setFpitch(
-                    f'{int(fpitch)} Hz ({self.freq_to_note(fpitch)})',
-                    self.normHorizontalScaleTransform.toScreen(fpitch)
+                f"{int(fpitch)} Hz ({self.freq_to_note(fpitch)})",
+                self.normHorizontalScaleTransform.toScreen(fpitch),
             )
 
             M = np.max(y)
             m = self.normVerticalScaleTransform.coord_min
-            y_int = (y-m)/(np.abs(M-m)+1e-3)
+            y_int = (y - m) / (np.abs(M - m) + 1e-3)
 
             x_left = zeros(x.shape)
             x_right = zeros(x.shape)
             x_left[0] = 1e-10
-            x_left[1:] = (x[1:] + x[:-1]) / 2.
+            x_left[1:] = (x[1:] + x[:-1]) / 2.0
             x_right[:-1] = x_left[1:]
             x_right[-1] = float(SAMPLING_RATE / 2)
             scaled_x_left = self.normHorizontalScaleTransform.toScreen(x_left)
             scaled_x_right = self.normHorizontalScaleTransform.toScreen(x_right)
 
-            baseline = 1. if self._baseline == Baseline.PLOT_BOTTOM else (1. - self.normVerticalScaleTransform.toScreen(0.))
+            baseline = (
+                1.0
+                if self._baseline == Baseline.PLOT_BOTTOM
+                else (1.0 - self.normVerticalScaleTransform.toScreen(0.0))
+            )
 
-            scaled_y = 1. - self.normVerticalScaleTransform.toScreen(y)
+            scaled_y = 1.0 - self.normVerticalScaleTransform.toScreen(y)
             z = y_int
-            self._curve_signal.setData(scaled_x_left, scaled_x_right, scaled_y, z, baseline)
+            self._curve_signal.setData(
+                scaled_x_left, scaled_x_right, scaled_y, z, baseline
+            )
 
             if self.peaks_enabled:
                 self.compute_peaks(y)
-                scaled_peak = 1. - self.normVerticalScaleTransform.toScreen(self.peak)
+                scaled_peak = 1.0 - self.normVerticalScaleTransform.toScreen(self.peak)
                 z_peak = self.peak_int
-                self._curve_peak.setData(scaled_x_left, scaled_x_right, scaled_peak, z_peak, baseline)
+                self._curve_peak.setData(
+                    scaled_x_left, scaled_x_right, scaled_peak, z_peak, baseline
+                )
 
     def draw(self):
         return
@@ -201,20 +225,20 @@ class SpectrumPlotWidget(QtWidgets.QWidget):
     def compute_peaks(self, y):
         if len(self.peak) != len(y):
             y_ones = ones(y.shape)
-            self.peak = y_ones * (-500.)
+            self.peak = y_ones * (-500.0)
             self.peak_int = zeros(y.shape)
-            self.peak_decay = y_ones * 20. * log10(PEAK_DECAY_RATE) * 5000
+            self.peak_decay = y_ones * 20.0 * log10(PEAK_DECAY_RATE) * 5000
 
-        mask1 = (self.peak < y)
-        mask2 = (~mask1)
+        mask1 = self.peak < y
+        mask2 = ~mask1
         mask2_a = mask2 * (self.peak_int < 0.2)
         mask2_b = mask2 * (self.peak_int >= 0.2)
 
         self.peak[mask1] = y[mask1]
         self.peak[mask2_a] = self.peak[mask2_a] + self.peak_decay[mask2_a]
 
-        self.peak_decay[mask1] = 20. * log10(PEAK_DECAY_RATE) * 5000
-        self.peak_decay[mask2_a] += 20. * log10(PEAK_DECAY_RATE) * 5000
+        self.peak_decay[mask1] = 20.0 * log10(PEAK_DECAY_RATE) * 5000
+        self.peak_decay[mask2_a] += 20.0 * log10(PEAK_DECAY_RATE) * 5000
 
-        self.peak_int[mask1] = 1.
+        self.peak_int[mask1] = 1.0
         self.peak_int[mask2_b] *= 0.975
