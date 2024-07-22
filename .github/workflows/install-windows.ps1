ae77e2aa053fb44397af3e8424a157f309009014
Timothée Lecomte <lecomte.timothee@gmail.com>
2024-07-22 20:29:26 +0200

    chore(deps): update PyInstaller
    PyInstaller 5.12+ should fix an error with Python 3.11.4+
    See https://github.com/pyinstaller/pyinstaller/pull/7694

--- .github/workflows/install-windows.ps1
+++ .github/workflows/install-windows.ps1
@@ -85,7 +85,7 @@ Write-Host "==========================================="
 Write-Host "Installing pyinstaller"
 Write-Host "==========================================="
 
-& pip install -U pyinstaller==5.7.0
+& pip install -U pyinstaller==6.9.0
 
 Write-Host ""
 Write-Host "==========================================="
