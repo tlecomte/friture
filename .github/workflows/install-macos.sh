e86a22e224fe31294adb60ac95a8ac9e05ca5c16
Timothée Lecomte <lecomte.timothee@gmail.com>
2024-10-28 16:36:35 +0100

    chore: move PyInstaller to pyproject.toml
    This will allow dependabot to update it automatically.

--- .github/workflows/install-macos.sh
+++ .github/workflows/install-macos.sh
@@ -15,8 +15,6 @@ python3 setup.py build_ext --inplace
 # see: https://github.com/tlecomte/friture/issues/154
 brew install portaudio --HEAD
 
-pip3 install -U pyinstaller==6.11.0
-
 pyinstaller friture.spec -y
 
 ls -la dist/*
